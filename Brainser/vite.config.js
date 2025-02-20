1fb5397cc7ef5d7af33d3918262a4a72aa814a12
faras <feras4gamer@gmail.com>
2025-02-20 21:36:33 +0300

    changed links

--- Brainser/vite.config.js
+++ Brainser/vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0', // Allow access from all network interfaces
-    port: 5173,
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
   },
-})
+});
