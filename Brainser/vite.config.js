94d4a84db5fc286f91c788a1518109e77a27d1c1
faras <feras4gamer@gmail.com>
2025-02-20 22:17:07 +0300

    updated

--- Brainser/vite.config.js
+++ Brainser/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
+  base: '/Brainser/', // Set this to your repository name
   plugins: [react()],
   build: {
     outDir: 'dist',
